ae21e6c3028e277baeae2db7049cf97942aeffef
ulitin.i <ulitin.i@milandr.ru>
2020-02-13 23:10:29 +0300

    Made an integer atomic.

--- src/main/java/ru/iaulitin/delayed/queue/service/user/UserService.java
+++ src/main/java/ru/iaulitin/delayed/queue/service/user/UserService.java
@@ -4,18 +4,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import ru.iaulitin.delayed.queue.processor.core.impl.user.UserQueueManager;
 import ru.iaulitin.delayed.queue.dao.UserDao;
 import ru.iaulitin.delayed.queue.entity.User;
+import ru.iaulitin.delayed.queue.processor.core.impl.user.UserQueueManager;
 
 import java.time.ZonedDateTime;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class UserService {
 
-    private static int usersCount = 0;//todo make atomicInteger
+    private static AtomicInteger usersCount = new AtomicInteger(0);
 
 
     private final UserDao userDao;
@@ -25,7 +26,7 @@ public class UserService {
     @Transactional
     public void createUser(long delayMillis) {
         User user = new User();
-        user.setName("User " + ++usersCount);
+        user.setName("User " + usersCount.incrementAndGet());
         user.setDeactivationTime(ZonedDateTime.now().plusSeconds(delayMillis / 1000));
         user.setActive(true);
 
